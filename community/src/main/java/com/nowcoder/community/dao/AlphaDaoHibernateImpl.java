be326a9ef8cc9bbf08cbc02724b04fb7470563ce
chenchangjun <23125018@bjtu.edu.cn>
2025-03-08 16:44:47 +0800

    第一次提交

---
+++ community/src/main/java/com/nowcoder/community/dao/AlphaDaoHibernateImpl.java
@@ -0,0 +1,11 @@
+package com.nowcoder.community.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository("alpha")
+public class AlphaDaoHibernateImpl implements AlphaDao {
+    @Override
+    public String select() {
+        return "Hibernate";
+    }
+}
